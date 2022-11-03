62065e59e251ffc637451613ea778c19e40940b0
Dianitsya <dianitasyanandamasta@gmail.com>
2022-11-03 15:34:58 +0700

    add tentukan banyak data

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -10,6 +10,23 @@ namespace Exercise_2
     {
         static void Main(string[] args)
         {
+            int i, n, dm, temp;
+            int[] tasya = new int[32];
+            {
+                while (true)
+                {
+                    //tentukan banyaknya data (n)
+                    Console.Write("Masukkan banyaknya elemen pada Array : ");
+                    string s = Console.ReadLine();
+                    n = Int32.Parse(s);
+                    if (n <= 32)
+                        break;
+                    else
+                        Console.WriteLine("\nArray dapat mempunyai maksimal 31 elemen.\n");
+                }
+            }
+
+        {
           
         }
     }
